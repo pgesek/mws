8e754bad028995276c777be3477a5b27ce3c6676
Paweł Gesek <pgesek@soldevelo.com>
2015-03-24 16:47:15 +0100

    Security

--- domain-module/src/main/java/com/soldevelo/motechdemo/domain/service/VisitService.java
+++ domain-module/src/main/java/com/soldevelo/motechdemo/domain/service/VisitService.java
@@ -1,6 +1,9 @@
 package com.soldevelo.motechdemo.domain.service;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+
 public interface VisitService {
 
+    @PreAuthorize("hasRole('visit-admin')")
     void createVisit(String patientId);
 }
